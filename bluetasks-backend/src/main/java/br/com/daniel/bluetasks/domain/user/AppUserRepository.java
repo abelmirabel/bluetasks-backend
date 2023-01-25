85ea0a28fbf9a5a4243c080a99ba31443b87dd7a
Daniel Moreira Franco Luiz <daniel.luiz@meta.com.br>
2023-01-25 14:06:11 -0300

    aula 20.7

--- bluetasks-backend/src/main/java/br/com/daniel/bluetasks/domain/user/AppUserRepository.java
+++ bluetasks-backend/src/main/java/br/com/daniel/bluetasks/domain/user/AppUserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
 	AppUser findByUsername(String username);
