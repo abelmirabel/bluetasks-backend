61c11ed9057234a4f7fb1aafb6d929d6d8b69be2
Daniel Moreira Franco Luiz <daniel.luiz@meta.com.br>
2023-01-25 14:21:49 -0300

    aula 20.9

--- bluetasks-backend/src/main/java/br/com/daniel/bluetasks/BluetasksBackendApplication.java
+++ bluetasks-backend/src/main/java/br/com/daniel/bluetasks/BluetasksBackendApplication.java
@@ -2,8 +2,12 @@ package br.com.daniel.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import br.com.daniel.bluetasks.domain.task.Task;
@@ -18,6 +22,19 @@ public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Task.class);
+	}
+	
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+	
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener vrel) {
+		Validator validator = validator();
+		vrel.addValidator("beforeCreate", validator);
+		vrel.addValidator("beforeSave", validator);
+		
 	}
 
 }
