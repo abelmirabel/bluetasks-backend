411a014d3286d6e36bd117b65fa3f48deab860a2
Daniel Moreira Franco Luiz <daniel.luiz@meta.com.br>
2023-01-25 14:02:52 -0300

    aula 20.6

--- bluetasks-backend/src/main/java/br/com/daniel/bluetasks/BluetasksBackendApplication.java
+++ bluetasks-backend/src/main/java/br/com/daniel/bluetasks/BluetasksBackendApplication.java
@@ -2,12 +2,22 @@ package br.com.daniel.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import br.com.daniel.bluetasks.domain.task.Task;
 
 @SpringBootApplication
-public class BluetasksBackendApplication {
+public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluetasksBackendApplication.class, args);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
